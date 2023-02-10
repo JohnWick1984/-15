53bfac7286b10a9dc6b5f8f415d0228b96f2c2f5
strupeiq <alinka-roxy@mail.ru>
2023-02-10 21:18:25 +0500

    Добавьте файлы проекта.

---
+++ 15.1/15.1.cpp
@@ -0,0 +1,42 @@
+﻿// 15.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(NULL));
+    int  n, m, mass;
+
+    cin >> n;
+    cin >> m;
+
+
+    int a[100][100];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            a[i][j] = 1 + rand() % 9;
+            cout << a[i][j];
+        }
+        cout << "\n";
+    }
+    
+    cout << "\n";
+    
+    
+    for (int k = 0; k < m; k++)
+    {
+        for (int v = 0; v < n; v++)
+        {
+            a[k][v] = a[v][k];
+            cout << a[v][k];
+        }
+        cout << "\n";
+    }
+}
+
---
+++ урок 15/урок 15.cpp
@@ -0,0 +1,38 @@
+﻿// урок 15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(NULL));
+    int  n, k;
+    cin >> n;
+  
+        int a[10][10];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int  j = 0; j < n; j++)
+            {
+                a[i][j] = 1 + rand() % 9;
+                cout << a[i][j];
+            }
+            cout << "\n";
+        }
+        cin >> k;
+
+
+        for (int r = 0; r < n; r++)
+        {
+            for (int l = 0; l < n; l++)
+            {
+                if (r - l - k == 0) cout << a[r][l] << "  ";
+              
+            }
+        }
+
+}
+
